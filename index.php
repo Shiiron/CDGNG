be770762683cdc23c9033e29d0f397d646afc9f6
Florestan Bredow <daiko@daiko.fr>
2017-02-20 16:31:43 +0100

    Corrige erreurs PHPMD et PHPCS

--- index.php
+++ index.php
@@ -20,51 +20,59 @@ include "./data/actions.php";
 include "./data/modalites.php";
 
 $model = new Model("config.php");
+$model->loadCalendarsList();
+
 $view = new View($model);
 
 $action = "";
-if (isset($_POST["action"]))
-	$action = $_POST["action"];
-
+if (isset($_POST["action"])) {
+    $action = $_POST["action"];
+}
 switch ($action) {
     case "Show":
-		if(isset($_POST["ics"]))
-			$view->showResults($_POST["ics"],
-							   $_POST["startDate"],
-							   $_POST["endDate"],
-							   $_POST["export"]);
-		else
-			print ("Aucun fichier n'a été sélectionné");
-		break;
+        if (isset($_POST["ics"])) {
+            $view->showResults(
+                $_POST["ics"],
+                $_POST["startDate"],
+                $_POST["endDate"],
+                $_POST["export"]
+            );
+            break;
+        }
+        print ("Aucun fichier n'a été sélectionné");
+        break;
 
-	case "Export":
-		if(isset($_POST["ics"]))
-			$view->showCsv($_POST["ics"],
-                           $_POST["startDate"],
-                           $_POST["endDate"],
-                           $_POST["export"]);
-		else
-			print ("Aucun fichier n'a été sélectionné");
-		break;
+    case "Export":
+        if (isset($_POST["ics"])) {
+            $view->showCsv(
+                $_POST["ics"],
+                $_POST["startDate"],
+                $_POST["endDate"],
+                $_POST["export"]
+            );
+            break;
+        }
+        print ("Aucun fichier n'a été sélectionné");
+        break;
 
-	case "Realised":
-		$_POST['codes'] = array('Tous');
-		if(isset($_POST["ics"]))
-			$view->showRealised($_POST["ics"],
-                           $_POST["date"]);
-		else
-			print ("Aucun fichier n'a été sélectionné");
-		break;
+    case "Realised":
+        $_POST['codes'] = array('Tous');
+        if (isset($_POST["ics"])) {
+            $view->showRealised($_POST["ics"], $_POST["date"]);
+            break;
+        }
+        print ("Aucun fichier n'a été sélectionné");
+        break;
 
-	case "tableauAction":
-		$view->exportTableauCDG("actions", isset($_POST["showArchived"]));
-		break;
+    case "tableauAction":
+        $view->exportTableauCDG("actions", isset($_POST["showArchived"]));
+        break;
 
-	case "tableauModalite":
-		$view->exportTableauCDG("modalites");
-		break;
+    case "tableauModalite":
+        $view->exportTableauCDG("modalites");
+        break;
 
-	default:
+    default:
         $view = new Views\Main($model);
         $view->show();
         break;
