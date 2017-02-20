a4e8e196b1372e3d7a6f7ed421c46c654923e5d9
Florestan Bredow <daiko@daiko.fr>
2017-02-20 16:16:59 +0100

    Corrige erreurs PHPMD et PHPCS

--- app/Model.php
+++ app/Model.php
@@ -9,159 +9,164 @@ namespace CDGNG;
  *
  * @version GIT: $Id$
  */
-class Model {
+class Model
+{
 
-	private $config;
-	private $calendars;
+    private $config;
+    private $calendars;
 
 
-	/**
-	 * Constructeur
-	 *
-	 * @param string $config_path Path to config file
-	 */
-	function __construct($config_path) {
-		include_once($config_path);
-		$this->calendars = array();
-	}
+    /**
+     * Constructeur
+     *
+     * @param string $configPath Path to config file
+     */
+    public function __construct($configPath)
+    {
+        include_once($configPath);
+        $this->calendars = array();
+    }
 
-	/**
-	 * Load calendars list
-	 *
-	 * @return array Calendars list
-	 */
-	function getCalList() {
+    /**
+     * Load calendars list
+     *
+     * @return array Calendars list
+     */
+    public function getCalList()
+    {
 
-		$result = array();
+        $result = array();
 
-		//Remplissage du tableau
-		if ($handle = opendir($this->config['calendars_path'])) {
-			while (false !== ($entry = readdir($handle))) {
-				if (substr_compare($entry, ".ics", -4, 4, TRUE) == 0) {
-					$path = $this->config['calendars_path'] . $entry;
-					if (is_file($path)) {
-						$result[basename($path, '.ics')] = $path;
-					}
-				}
-			}
-			closedir($handle);
-		}
+        //Remplissage du tableau
+        if ($handle = opendir($this->config['calendars_path'])) {
+            while (false !== ($entry = readdir($handle))) {
+                if (substr_compare($entry, ".ics", -4, 4, true) == 0) {
+                    $path = $this->config['calendars_path'] . $entry;
+                    if (is_file($path)) {
+                        $result[basename($path, '.ics')] = $path;
+                    }
+                }
+            }
+            closedir($handle);
+        }
 
-		//Tri du tableau
-		ksort($result);
+        //Tri du tableau
+        ksort($result);
 
-		return $result;
-	}
+        return $result;
+    }
 
-	/**
-	 * Make timestamp from date string (start of day)
-	 *
-	 * @param string $str date as string
-	 *
-	 * @return int timestamp
-	 */
-	function strToTime($str) {
-		$tab = explode("-", $str, 3);
-		return strtotime($tab[2] . "-" . $tab[1] . "-" . $tab[0]);
-	}
+    /**
+     * Make timestamp from date string (start of day)
+     *
+     * @param string $str date as string
+     *
+     * @return int timestamp
+     */
+    public function strToTime($str)
+    {
+        $tab = explode("-", $str, 3);
+        return strtotime($tab[2] . "-" . $tab[1] . "-" . $tab[0]);
+    }
 
-	/**
-	 * Make timestamp from date string (End of day)
-	 *
-	 * @param string $str date as string
-	 *
-	 * @return int timestamp
-	 */
-	function strToTime_EndDate($str) {
-		$tab = explode("-", $str, 3);
-		$time = mktime(23, 59, 59, $tab[1], $tab[0], $tab[2]);
-		return $time;
-	}
+    /**
+     * Make timestamp from date string (End of day)
+     *
+     * @param string $str date as string
+     *
+     * @return int timestamp
+     */
+    public function strToTimeEndDate($str)
+    {
+        $tab = explode("-", $str, 3);
+        $time = mktime(23, 59, 59, $tab[1], $tab[0], $tab[2]);
+        return $time;
+    }
 
-	/**
-	 * Analyse one or more calendars
-	 *
-	 * @param string $cal_path Path to calendar
-	 * @param int    $ts_start Start of event timestamp
-	 * @param int 	 $ts_end   End of event timestamp
-	 */
-	function analyseCal($cal_path, $ts_start, $ts_end) {
-		//Reset tab
-		$this->actions = array();
-		$this->modalites = array();
-		$this->total = 0;
-		$this->tabError = array();
+    /**
+     * Analyse one or more calendars
+     *
+     * @param string $calPath Path to calendar
+     * @param int    $tsStart Start of event timestamp
+     * @param int      $tsEnd   End of event timestamp
+     */
+    public function analyseCal($calPath, $tsStart, $tsEnd)
+    {
+        //Reset tab
+        $this->actions = array();
+        $this->modalites = array();
+        $this->total = 0;
+        $this->tabError = array();
 
-		$ts_start = $this->strToTime($ts_start);
-		$ts_end = $this->strToTime_EndDate($ts_end);
+        $tsStart = $this->strToTime($tsStart);
+        $tsEnd = $this->strToTimeEndDate($tsEnd);
 
-		//Parse each calendar
-		foreach ($cal_path as $path) {
-			$cal = new Calendar($path);
-			$name = $cal->getname();
-			$this->calendars[$name] = $cal;
-			$this->calendars[$name]->parse($ts_start, $ts_end);
-		}
-	}
+        //Parse each calendar
+        foreach ($calPath as $path) {
+            $cal = new Calendar($path);
+            $name = $cal->getname();
+            $this->calendars[$name] = $cal;
+            $this->calendars[$name]->parse($tsStart, $tsEnd);
+        }
+    }
 
-	/**
-	 * Returns errors for all calendars
-	 *
-	 * @return array All calendars errors
-	 */
-	function getErrors(){
-		$output = array();
-		foreach ($this->calendars as $name => $calendar) {
-			$output[$calendar->getName()] = $calendar->getErrors();
-		}
-		return $output;
-	}
+    /**
+     * Returns errors for all calendars
+     *
+     * @return array All calendars errors
+     */
+    public function getErrors()
+    {
+        $output = array();
+        foreach ($this->calendars as $name => $calendar) {
+            $output[$name] = $calendar->getErrors();
+        }
+        return $output;
+    }
 
-	/**
-	 * Return calendar's data
-	 *
-	 * @param string $type Result by action or by modalites (byAction, byModalite)
-	 * @param string $slot define time slot (day, week, month, year, all)
-	 * @param bool $fusion Définis si il faut fusionner les données des différents calendriers
-	 */
+    /**
+     * Return calendar's data
+     *
+     * @param string $type Result by action or by modalites (byAction, byModalite)
+     * @param string $slot define time slot (day, week, month, year, all)
+     * @param bool $fusion Définis si il faut fusionner les données des différents calendriers
+     */
 
-	function getData($slot = "All", $fusion = false){
-		$output = array();
-		if(!$fusion){
-			foreach ($this->calendars as $name => $calendar) {
-				$output[$calendar->getName()] = $calendar->getData($slot);
-				$output[$calendar->getName()]['duration'] = $calendar->getTotalLength();
-			}
-		} else {
-			//TODO : need addCal dans Calendar
-		}
-		return $output;
-	}
+    public function getData($slot = "All")
+    {
+        $output = array();
+        foreach ($this->calendars as $name => $calendar) {
+            $output[$name] = $calendar->getData($slot);
+            $output[$name]['duration'] = $calendar->getTotalLength();
+        }
+        return $output;
+    }
 
-	/**
-	 * Calculate sum of duration of all valid events
-	 *
-	 * @return int Duration of all valid events for all selected calendars
-	 */
-	function getTotal(){
-		$total = 0;
+    /**
+     * Calculate sum of duration of all valid events
+     *
+     * @return int Duration of all valid events for all selected calendars
+     */
+    public function getTotal()
+    {
+        $total = 0;
+        foreach ($this->calendars as $calendar) {
+            $total += $calendar->getTotalLength();
+        }
+        return $total;
+    }
 
-		foreach ($this->calendars as $name => $calendar) {
-			$total += $calendar->getTotalLength();
-		}
-		return $total;
-	}
-
-	/**
-	 * Return name of selected calendars
-	 *
-	 * @return string
-	 */
-	function GetName(){
-		$output = "";
-		foreach ($this->calendars as $name => $calendar) {
-			$output .= $calendar->getName()."+";
-		}
-		return substr($output, 0, -1);
-	}
+    /**
+     * Return name of selected calendars
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        $output = "";
+        foreach ($this->calendars as $calendar) {
+            $output .= $calendar->name."+";
+        }
+        return substr($output, 0, -1);
+    }
 }
