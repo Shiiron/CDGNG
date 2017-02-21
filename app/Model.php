14caa44c7edeada13ff1d00e2455231ba4493c43
Florestan Bredow <daiko@daiko.fr>
2017-02-21 12:17:36 +0100

    fixup

--- app/Model.php
+++ app/Model.php
@@ -38,7 +38,6 @@ class Model
         $this->calendars = array();
         $calPath = $this->config['calendars_path'];
 
-        //Remplissage du tableau
         if ($handle = opendir($calPath)) {
             while (false !== ($entry = readdir($handle))) {
                 if (substr_compare($entry, ".ics", -4, 4, true) === 0) {
@@ -51,63 +50,7 @@ class Model
             }
             closedir($handle);
         }
-        //Tri du tableau
+
         ksort($this->calendars);
     }
-
-    /**
-     * Returns errors for all calendars
-     *
-     * @return array All calendars errors
-     */
-    public function getErrors()
-    {
-        $output = array();
-        foreach ($this->calendars as $name => $calendar) {
-            $output[$name] = $calendar->getErrors();
-        }
-        return $output;
-    }
-
-    /**
-     * Return name of selected calendars
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        $output = "";
-        foreach ($this->calendars as $calendar) {
-            $output .= $calendar->name."+";
-        }
-        return substr($output, 0, -1);
-    }
-
-    /**
-     * Make timestamp from date string (start of day)
-     *
-     * @param string $str date as string
-     *
-     * @return int timestamp
-     */
-    private function strToTime($str)
-    {
-        $tab = explode("-", $str, 3);
-        return strtotime($tab[2] . "-" . $tab[1] . "-" . $tab[0]);
-    }
-
-    /**
-     * Make timestamp from date string (End of day)
-     *
-     * @param string $str date as string
-     *
-     * @return int timestamp
-     */
-    private function strToTimeEndDate($str)
-    {
-        $tab = explode("-", $str, 3);
-        $time = mktime(23, 59, 59, $tab[1], $tab[0], $tab[2]);
-        return $time;
-    }
-
 }
